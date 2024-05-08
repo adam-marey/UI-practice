c3f9ec7efc44d53c19ad52f23216b083bc859ede
adam <adammarey51@gmail.com>
2024-05-08 10:08:25 -0700

    code highlight setup, tested

--- src/components/Learn/Learn.jsx
+++ src/components/Learn/Learn.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Learn.css';
-
+import CodeSnippet from '../CodeHighlight/CodeSnippet';
 const Learn = () => {
+  const javascriptCode = `const greeting = "Hello, world!";`;
   return (
     <div className="learn-container">
       <div className="side-menu" />
       <div className="learn-content">
         <h1>Learning Resources</h1>
+        <CodeSnippet code={javascriptCode} language="javascript" />
       </div>
     </div>
   );
