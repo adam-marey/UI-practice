e06fd9c68812686f76a604786a404798784ff8eb
adam <adammarey51@gmail.com>
2024-05-08 09:49:27 -0700

    learn container css math puzzle completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Blog from './components/Blog/Blog'
-import Navbar from './components/Navbar/Navbar'
-import Learn from './components/Learn/Learn'
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Blog from './components/Blog/Blog';
+import Navbar from './components/Navbar/Navbar';
+import Learn from './components/Learn/Learn';
 function App() {
-
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path="/blog" element={<Blog/>} />
-      <Route path="/learn" element={<Learn/>} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/learn" element={<Learn />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Learn/Learn.jsx
+++ src/components/Learn/Learn.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import './Learn.css';
 
 const Learn = () => {
-  return <div>Learn</div>;
+  return (
+    <div className="learn-container">
+      <div className="side-menu" />
+      <div className="learn-content">
+        <h1>Learning Resources</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Learn;
